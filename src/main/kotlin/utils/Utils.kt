0542c43fd37cb9eaafaed4662b5a67b53e22c51d
Jon Senchyna <jsenchyna@clearcover.com>
2022-12-01 06:14:02 -0500

    Update `readInput` to read from resources

--- src/main/kotlin/utils/Utils.kt
+++ src/main/kotlin/utils/Utils.kt
@@ -5,12 +5,13 @@ import java.security.MessageDigest
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
+fun readInput(day: Int, name: String) = "$day".padStart(2, '0').let { dir ->
+  {}.javaClass.getResourceAsStream("day$dir/$name.txt")!!.bufferedReader().readLines()
+}
 
 /**
  * Converts string to md5 hash.
  */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+  .toString(16)
+  .padStart(32, '0')
