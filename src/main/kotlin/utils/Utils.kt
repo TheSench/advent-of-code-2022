50eefac93aa45dc3a10dee11388b169b537627b5
Jon Senchyna <jsenchyna@clearcover.com>
2022-12-01 06:33:45 -0500

    Implement day1

--- src/main/kotlin/utils/Utils.kt
+++ src/main/kotlin/utils/Utils.kt
@@ -1,4 +1,3 @@
-import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
@@ -7,6 +6,27 @@ import java.security.MessageDigest
  */
 fun readInput(day: Int, name: String) = "$day".padStart(2, '0').let { dir ->
   {}.javaClass.getResourceAsStream("day$dir/$name.txt")!!.bufferedReader().readLines()
+}
+
+fun List<String>.groupByBlanks(): List<List<String>> {
+  var current: List<String> = listOf()
+  var groups: List<List<String>> = listOf()
+  for (line: String in this) {
+    if (line.isBlank()) {
+      groups = groups.plusElement(current)
+      current = emptyList()
+    } else {
+      current = current + line
+    }
+  }
+  if (!current.isEmpty()) {
+    groups = groups.plusElement(current)
+  }
+  return groups
+}
+
+fun <T, R> List<List<T>>.mapGroups(transform: (T) -> R) = this.map {
+  it.map(transform)
 }
 
 /**
