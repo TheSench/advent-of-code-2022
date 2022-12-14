5941bb7676a2cc8e50030abf7f01064b2aa64adb
Jon Senchyna <TheSench@users.noreply.github.com>
2022-12-14 05:19:24 -0500

    chore: Scaffold day 14

---
+++ src/main/kotlin/day14/Day14.kt
@@ -0,0 +1,16 @@
+package day14
+
+import runDay
+
+fun main() {
+    fun part1(input: List<String>) = 0
+
+    fun part2(input: List<String>) = 0
+
+    (object {}).runDay(
+        part1 = ::part1,
+        part1Check = 13,
+        part2 = ::part2,
+        part2Check = 140,
+    )
+}
