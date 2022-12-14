2dc716dee986238d7ff95c6dd5bbe5c996fdd729
Jon Senchyna <TheSench@users.noreply.github.com>
2022-12-14 05:25:39 -0500

    feat: Parse input to points

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -1,6 +1,7 @@
 package day14
 
 import runDay
+import utils.Point
 
 fun main() {
     fun part1(input: List<String>) = 0
@@ -14,3 +15,10 @@ fun main() {
         part2Check = 140,
     )
 }
+
+fun String.toLines() = split(" -> ")
+    .map {
+        it.split(",", limit = 2)
+            .map { num -> num.toInt() }
+    }
+    .map { nums -> Point(nums.first(), nums.last()) }
--- src/test/kotlin/day14/Day14Test.kt
+++ src/test/kotlin/day14/Day14Test.kt
@@ -1,4 +1,23 @@
 package day14
 
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import utils.Point
+
 class Day14Test {
+    @Nested
+    @Suppress("ClassName")
+    inner class `String#toLines() should` {
+        @Test
+        fun `parse the input into a series of points`() {
+            val input = "498,4 -> 498,6 -> 496,6"
+
+            input.toLines() shouldBe listOf(
+                Point(498, 4),
+                Point(498, 6),
+                Point(496, 6),
+            )
+        }
+    }
 }
