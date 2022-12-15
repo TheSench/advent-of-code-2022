bf0deff6d8e13d6961b4c1ad410b84413285645e
Jon Senchyna <TheSench@users.noreply.github.com>
2022-12-14 23:30:03 -0500

    feat: Implement day14, part 1

--- src/main/kotlin/day14/Day14.kt
+++ src/main/kotlin/day14/Day14.kt
@@ -8,12 +8,14 @@ fun main() {
         input
             .map { it.toLine() }
             .toGrid()
+            .dropSand()
+            .size
 
     fun part2(input: List<String>) = 0
 
     (object {}).runDay(
         part1 = ::part1,
-        part1Check = 13,
+        part1Check = 24,
         part2 = ::part2,
         part2Check = 140,
     )
@@ -21,6 +23,7 @@ fun main() {
 
 typealias Line = List<Point>
 typealias Lines = List<Line>
+typealias Grid = Set<Point>
 
 fun String.toLine() = split(" -> ")
     .map {
@@ -42,3 +45,32 @@ fun Line.addToGrid(grid: MutableSet<Point>) {
         }
     }
 }
+
+fun Grid.dropSand(start: Point = Point(500, 0)): Set<Point> {
+    val stones = this
+    val sand = mutableSetOf<Point>()
+    val maxY = stones.maxOf { it.y }
+    val pointsToRevisit = ArrayDeque<Point>()
+    var pointConsidering = start
+    while (pointConsidering.y < maxY) {
+        val next = pointConsidering.next(stones, sand)
+        pointConsidering = when (next) {
+            null -> {
+                sand.add(pointConsidering)
+                pointsToRevisit.removeFirst()
+            }
+
+            else -> {
+                pointsToRevisit.addFirst(pointConsidering)
+                next
+            }
+        }
+    }
+    return sand
+}
+
+fun Point.next(stones: Grid, sand: Grid) = listOf(
+    this + Point(0, 1),
+    this + Point(-1, 1),
+    this + Point(1, 1),
+).firstOrNull { it !in stones && it !in sand }
