434454c4c44d53aeee1ef64178aeb57163f39634
Jon Senchyna <TheSench@users.noreply.github.com>
2022-12-15 08:04:38 -0500

    feat: Parse input lines

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -1,9 +1,10 @@
 package day15
 
 import runDay
+import utils.Point
 
 fun main() {
-    fun part1(input: List<String>) = 0
+    fun part1(input: List<String>) = input
 
     fun part2(input: List<String>) = 0
 
@@ -14,3 +15,19 @@ fun main() {
         part2Check = 93,
     )
 }
+
+data class ParsedLine(val sensor: Point, val beacon: Point)
+
+fun String.parse(): ParsedLine = split(": ", limit = 2).let {
+    val sensor = it.first().toPoint()
+    val relativeBeacon = it.last().toPoint()
+    return ParsedLine(
+        sensor,
+        sensor + relativeBeacon,
+    )
+}
+
+val pointRegex = Regex("""x=(-?\d+), y=(-?\d+)""")
+fun String.toPoint(): Point = pointRegex.find(this)!!
+    .destructured
+    .let { (x, y) -> Point(x.toInt(), y.toInt()) }
--- src/test/kotlin/day15/Day15Test.kt
+++ src/test/kotlin/day15/Day15Test.kt
@@ -1,4 +1,39 @@
 package day15
 
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import utils.Point
+
 class Day15Test {
+    @Nested
+    @Suppress("ClassName")
+    inner class `String#parse() should` {
+        @Test
+        fun `parse a line into the coordinates for a sensor and its relative beacon`() {
+            val line = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15"
+
+            line.parse() shouldBe ParsedLine(
+                Point(2, 18),
+                Point(0, 33),
+            )
+        }
+    }
+    @Nested
+    @Suppress("ClassName")
+    inner class `String#toPoint() should` {
+        @Test
+        fun `parse a string into a point`() {
+            val line = "Something at x=9, y=6"
+
+            line.toPoint() shouldBe Point(9, 6)
+        }
+
+        @Test
+        fun `parse a string with negative coordinates`() {
+            val line = "Something at x=-3, y=-5"
+
+            line.toPoint() shouldBe Point(-3, -5)
+        }
+    }
 }
