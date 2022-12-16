03528e8d7acab61cdf20ac379ec7a13f5dc6edc9
Jon Senchyna <TheSench@users.noreply.github.com>
2022-12-16 07:17:58 -0500

    feat: Parse input

--- src/main/kotlin/day16/Day16.kt
+++ src/main/kotlin/day16/Day16.kt
@@ -3,14 +3,37 @@ package day16
 import runDay
 
 fun main() {
-    fun part1(input: List<String>) = 0
+    fun part1(input: List<String>) = input.parse()
 
     fun part2(input: List<String>) = 0
 
     (object {}).runDay(
         part1 = ::part1,
-        part1Check = -1,
+        part1Check = 1651,
         part2 = ::part2,
         part2Check = -1,
+    )
+}
+
+data class Room(
+    val name: String,
+    val flowRate: Int,
+    val connected: List<String>
+)
+
+fun List<String>.parse() = map { it.toRoom() }.associateBy { it -> it.name }
+
+val roomRegex = Regex("""Valve (\w+) has flow rate=([\d-]+); tunnels? leads? to valves? (.*)""")
+fun String.toRoom(): Room {
+    val match = roomRegex.matchEntire(this)
+    val (
+        name,
+        rate,
+        connected,
+    ) = match!!.destructured
+    return Room(
+        name,
+        rate.toInt(),
+        connected.split(", ")
     )
 }
