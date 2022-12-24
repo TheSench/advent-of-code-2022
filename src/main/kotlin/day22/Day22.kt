b79c879e6cff890d5421067dd7dcf46d8f6a7219
Jon Senchyna <TheSench@users.noreply.github.com>
2022-12-24 13:26:58 -0500

    refactor: Extract Ring

--- src/main/kotlin/day22/Day22.kt
+++ src/main/kotlin/day22/Day22.kt
@@ -5,6 +5,7 @@ import runDay
 import stackOf
 import toPair
 import utils.Point
+import utils.Ring
 import java.util.function.Predicate
 
 fun main() {
@@ -229,57 +230,6 @@ open class Face(
     operator fun contains(point: Point) = point in tiles
     operator fun get(point: Point) = tiles[point]
 }
-
-class RingNode<T>(val value: T) {
-    lateinit var previous: RingNode<T>
-    lateinit var next: RingNode<T>
-
-    companion object {
-        operator fun <T> invoke(vararg values: T): RingNode<T> {
-            val nodes = values.map(::RingNode)
-            val head = nodes.first()
-            nodes.reduce { previous, next ->
-                previous.next = next
-                next.previous = previous
-                next
-            }
-            val last = nodes.last()
-            last.next = head
-            head.previous = last
-            return head
-        }
-    }
-
-    fun find(value: T): RingNode<T> {
-        return if (this.value == value) {
-            this
-        } else {
-            next.find(value)
-        }
-    }
-
-    operator fun iterator(): Iterator<T> {
-        return RingIterator(this)
-    }
-}
-
-class RingIterator<T>(private val start: RingNode<T>) : Iterator<T> {
-
-    private var current = start
-    private var first = true
-
-    override fun hasNext(): Boolean {
-        return current != start || first
-    }
-
-    override fun next(): T {
-        first = false
-        current = current.next
-        return current.value
-    }
-}
-
-typealias Ring<T> = RingNode<T>
 
 data class FaceSide(val region: Int, val edge: Direction)
 
---
+++ src/main/kotlin/utils/Ring.kt
@@ -0,0 +1,52 @@
+package utils
+
+class RingNode<T> private constructor(val value: T) {
+    lateinit var previous: RingNode<T>
+    lateinit var next: RingNode<T>
+
+    companion object {
+        operator fun <T> invoke(vararg values: T): RingNode<T> {
+            val nodes = values.map(::RingNode)
+            val head = nodes.first()
+            nodes.reduce { previous, next ->
+                previous.next = next
+                next.previous = previous
+                next
+            }
+            val last = nodes.last()
+            last.next = head
+            head.previous = last
+            return head
+        }
+    }
+
+    fun find(value: T): RingNode<T> {
+        return if (this.value == value) {
+            this
+        } else {
+            next.find(value)
+        }
+    }
+
+    operator fun iterator(): Iterator<T> {
+        return RingIterator(this)
+    }
+}
+
+class RingIterator<T>(private val start: RingNode<T>) : Iterator<T> {
+
+    private var current = start
+    private var first = true
+
+    override fun hasNext(): Boolean {
+        return current != start || first
+    }
+
+    override fun next(): T {
+        first = false
+        current = current.next
+        return current.value
+    }
+}
+
+typealias Ring<T> = RingNode<T>
