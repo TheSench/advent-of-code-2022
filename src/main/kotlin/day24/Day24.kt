d82508825910b84c76173185581ce37829302d42
Jon Senchyna <TheSench@users.noreply.github.com>
2022-12-25 08:04:58 -0500

    feat: Implement part 1

--- src/main/kotlin/day24/Day24.kt
+++ src/main/kotlin/day24/Day24.kt
@@ -1,34 +1,90 @@
 package day24
 
 import runDay
-import stackOf
 import utils.Point
 
 fun main() {
 
-    fun part1(input: List<String>) = input.parse().let { (boundaries, blizzards) ->
-        val positions = stackOf(Point(1, 0))
-        while (positions.isNotEmpty()) {
-            blizzards
+    fun part1(input: List<String>) = input.parse().let { (bounds, blizzards) ->
+        var positions = listOf(bounds.start)
+        var currentBlizzards = blizzards
+        val lcm = lcm(bounds.width, bounds.height)
+        val seen = mutableSetOf(0 to bounds.start)
+        repeat(Int.MAX_VALUE) { turn ->
+            currentBlizzards = currentBlizzards.move(bounds)
+            val blizzardLocations = currentBlizzards.map { it.location }.toSet()
+            positions = positions.flatMap { location ->
+                location.getMoves().filter {
+                    it in bounds && location !in blizzardLocations
+                }.filter {
+                    (turn % lcm) to it !in seen
+                }.also { moves ->
+                    if (moves.any { it == bounds.end }) {
+                        return (turn + 2)
+                    }
+                    moves.forEach {
+                        seen.add((turn % lcm) to it)
+                    }
+                }
+            }
         }
-        0
+        Int.MAX_VALUE
     }
 
     fun part2(input: List<String>) = 0
 
     (object {}).runDay(
         part1 = ::part1,
-        part1Check = -1,
+        part1Check = 18,
         part2 = ::part2,
         part2Check = -1,
     )
 }
 
+fun debug(turn: Int, bounds: Bounds, allBlizzards: List<Blizzard>) {
+    val blizzardLocations = allBlizzards.groupBy { it.location }
+    val map = (0..bounds.height).map { y ->
+        (0..bounds.width).map { x ->
+            val point = Point(x, y)
+            when {
+                x == 0 || y == 0 || x == bounds.width || y == bounds.height -> '#'
+                point in blizzardLocations -> {
+                    val blizzards = blizzardLocations[point]!!
+                    when (blizzards.size) {
+                        1 -> blizzards[0].toString()
+                        in (2..9) -> blizzards.size
+                        else -> '@'
+                    }
+                }
+
+                else -> ' '
+            }
+        }.joinToString("")
+    }.joinToString("\n")
+
+    println("After turn ${turn + 1}")
+    println(map)
+    println()
+}
+
+operator fun Bounds.contains(point: Point) =
+    (point.x in (1 until width) && point.y in (1 until height))
+            || point == start
+            || point == end
+
+fun Point.getMoves() = listOf(
+    copy(x = x + 1),
+    copy(x = x - 1),
+    copy(y = y + 1),
+    copy(y = y - 1),
+    this,
+)
+
 fun List<String>.parse() = getBoundaries() to toBlizzards()
 
 fun List<String>.getBoundaries() = Bounds(
-    width = this[0].length,
-    height = this.size,
+    width = this[0].length - 1,
+    height = this.size - 1,
 )
 
 fun List<String>.toBlizzards() = flatMapIndexed { y, row ->
@@ -38,18 +94,30 @@ fun List<String>.toBlizzards() = flatMapIndexed { y, row ->
             'v' -> Blizzard.down(x, y)
             '<' -> Blizzard.left(x, y)
             '>' -> Blizzard.right(x, y)
-            else -> null
+            '#', '.' -> null
+            else -> throw IllegalArgumentException("$char")
         }
     }
 }
 
-data class Bounds(val width: Int, val height: Int)
+data class Bounds(val width: Int, val height: Int) {
+    val start = Point(1, 0)
+    val end = Point(width - 1, height)
+}
 
 data class Blizzard(val location: Point, val velocity: Point) {
     val x get() = location.x
     val y get() = location.y
 
     fun move() = copy(location = location + velocity)
+
+    override fun toString(): String = when {
+        velocity.x > 0 -> ">"
+        velocity.x < 0 -> "<"
+        velocity.y > 0 -> "v"
+        velocity.y < 0 -> "^"
+        else -> "A"
+    }
 
     companion object {
         fun up(x: Int, y: Int) = Blizzard(Point(x, y), Point(0, -1))
@@ -68,3 +136,16 @@ fun List<Blizzard>.move(bounds: Bounds) = map { it.move() }.map {
         else -> it
     }
 }
+
+private fun gcd(a: Int, b: Int): Int {
+    var a = a
+    var b = b
+    while (b > 0) {
+        val temp = b
+        b = a % b // % is remainder
+        a = temp
+    }
+    return a
+}
+
+private fun lcm(a: Int, b: Int): Int = a * (b / gcd(a, b))
