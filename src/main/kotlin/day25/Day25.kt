85bcb94dc7c3ab238a1a5ccdc12012b4676903cb
Jon Senchyna <TheSench@users.noreply.github.com>
2022-12-25 17:20:03 -0500

    feat: Implement conversions

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -1,6 +1,7 @@
 package day25
 
 import runDay
+import kotlin.math.pow
 
 fun main() {
 
@@ -18,5 +19,36 @@ fun main() {
 
 typealias SNAFU = String
 typealias Base10 = Int
-fun SNAFU.toBase10(): Int = 0
-fun Base10.toSnafu(): String = "0"
+
+fun SNAFU.toBase10(): Int = reversed().foldIndexed(0) { index, base10, char ->
+    val next = when (char) {
+        '=' -> -2
+        '-' -> -1
+        '0' -> 0
+        '1' -> 1
+        '2' -> 2
+        else -> throw IllegalArgumentException("Invalid char: $char")
+    }
+
+    base10 + 5.pow(index) * next
+}
+
+fun Base10.toSnafu(): String = toInt().let { base10 ->
+    val chars = mutableListOf<Char>()
+    var remaining = base10
+    while (remaining > 0) {
+        val digit = remaining % 5
+        remaining = (remaining - digit) / 5
+        chars.add(when (digit) {
+            0 -> '0'
+            1 -> '1'
+            2 -> '2'
+            3 -> '='.also { remaining += 1 }
+            4 -> '-'.also { remaining += 1 }
+            else -> throw IllegalArgumentException("Unexpected number")
+        })
+    }
+    chars.reversed().joinToString("")
+}
+
+fun Int.pow(power: Int) = toDouble().pow(power).toInt()
