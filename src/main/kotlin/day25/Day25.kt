5786c2db3792dd390d921cd86b1d04b72b0915e2
Jon Senchyna <TheSench@users.noreply.github.com>
2022-12-25 20:49:08 -0500

    feat: Implement part 1

--- src/main/kotlin/day25/Day25.kt
+++ src/main/kotlin/day25/Day25.kt
@@ -5,15 +5,15 @@ import kotlin.math.pow
 
 fun main() {
 
-    fun part1(input: List<String>) = 0
+    fun part1(input: List<String>) = input.sumOf { it.toBase10() }.toSnafu()
 
     fun part2(input: List<String>) = 0
 
     (object {}).runDay(
         part1 = ::part1,
-        part1Check = 18,
+        part1Check = "2=-1=0",
         part2 = ::part2,
-        part2Check = 54,
+        part2Check = -1,
     )
 }
 
